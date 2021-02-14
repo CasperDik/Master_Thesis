14cf907093a9e50cb92c58dac0a8c58eeee12a1b
Casper Dik <casperdik3@gmail.com>
2021-02-14 15:29:07 +0100

    update

--- first_part/handling data.py
+++ first_part/handling data.py
@@ -6,30 +6,38 @@ import numpy as np
 df = pd.read_excel(r'C:\Users\Casper Dik\OneDrive\Documenten\MSc Finance\Master Thesis\Data\Carbon Prices\ca'
                              r'rbon_prices_full.xlsx')
 # rename column
-df.rename(columns={"time": "Date", "CFI2Zc1": "Carbon_price"}, inplace=True)
+df.rename(columns={"time": "Date", "CFI2Zc1": "price"}, inplace=True)
 
 # invert date and prices
 df.Date = df.Date.values[::-1]
-df.Carbon_price = df.Carbon_price.values[::-1]
+df.price = df.price.values[::-1]
 
 # check for missing values
 print(df.isnull().sum())
 
 # describe data
-print("descriptive stats carbon price full: \n", df["Carbon_price"].describe())
-print("mean from panda: ", df.Carbon_price.mean())
+print("descriptive stats carbon price full: \n", df["price"].describe())
+print("mean from panda: ", df.price.mean())
 
 # create return series
-df["daily_returns"] = df.Carbon_price.pct_change()
-print("descriptive stats daily returns: \n", df.daily_returns.describe())
+df["daily_returns"] = df.price.pct_change()
+df["log_returns"] = np.log(df.price) - np.log(df.price.shift(1))
+print(df.head())
 
-# new dataframe from 2009
+# new dataframe from 2008ish
 cp_2 = df.iloc[350:]
-print("descriptive stats carbon price partially: \n", cp_2["Carbon_price"].describe())
 
 # plot prices and returns
-fig, axs = plt.subplots(2)
-fig.suptitle('Carbon prices and returns')
-axs[0].plot(cp_2.Date, cp_2.Carbon_price)
+fig, axs = plt.subplots(3)
+axs[0].plot(cp_2.Date, cp_2.price)
 axs[1].plot(cp_2.Date, cp_2.daily_returns)
+axs[2].plot(cp_2.Date, cp_2.log_returns)
+
+axs[0].title.set_text("Price:")
+axs[1].title.set_text("Returns:")
+axs[2].title.set_text("Log returns:")
+
+axs[0].get_xaxis().set_visible(False)
+axs[1].get_xaxis().set_visible(False)
+
 plt.show()
