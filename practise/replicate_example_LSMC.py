7c7e996180a0e3ab0a1e6e195855a3d5b39c59df
Casper Dik <casperdik3@gmail.com>
2021-02-16 14:11:48 +0100

    update

--- practise/replicate_example_LSMC.py
+++ practise/replicate_example_LSMC.py
@@ -14,13 +14,14 @@ cf_matrix = np.zeros((4, 8))
 # parameters
 K = 1.1
 rf = 0.06
-# todo: replace 8's with variable for number of paths (but set it equal to 8)
-# todo: replace the indexes e.g. Y[2,i] --> 2 needs to be variable --> T=3, T-1
-# todo: loop T times? --> replace T-1 with T-t
+paths = 8
+T = 3
+t = 1
+# todo: build loop
 
 # cash flow matrix time 3
-for p in range(8):
-    cf_matrix[3, p] = max(0, K - (price_matrix[3, p]))
+for p in range(paths):
+    cf_matrix[T, p] = max(0, K - (price_matrix[T, p]))
 
 # find continuation value
 # X = price in time T-1, Y = pv cf
@@ -28,45 +29,44 @@ Y = np.copy(cf_matrix)
 X = np.copy(price_matrix)
 
 # discount cf t=3 to t=2
-for i in range(8):
-    Y[2, i] = cf_matrix[3, i] * np.exp(-rf)
+for i in range(paths):
+    Y[T-t, i] = cf_matrix[T, i] * np.exp(-rf)
 
 # delete columns that are out of the money in t=2
-for j in range(7, -1, -1):
-    if price_matrix[2, j] > K:
+for j in range(T-1, -1, -1):
+    if price_matrix[T-1, j] > K:
         Y = np.delete(Y, j, axis=1)
         X = np.delete(X, j, axis=1)
 
 # regress Y on constant, X, X^2
-regression = np.polyfit(X[2], Y[2], 2)
+regression = np.polyfit(X[T-t], Y[T-t], 2)
 # first is coefficient for X^2, second is coefficient X, third is constant
 beta_2 = regression[0]
 slope = regression[1]
 intercept = regression[2]
 
 # continuation value
-continuation_value = np.zeros((1, 8))
+continuation_value = np.zeros((1, paths))
 tick = 0
-for i in range(8):
-    if price_matrix[2, i] < K:
-        continuation_value[0, i] = intercept + slope * X[2, i-tick] + beta_2 * (X[2, i-tick] ** 2)
+for i in range(paths):
+    if price_matrix[T-t, i] < K:
+        continuation_value[0, i] = intercept + slope * X[T-t, i-tick] + beta_2 * (X[T-t, i-tick] ** 2)
     else:
         continuation_value[0, i] = 0
         tick += 1
 
 # compare immediate exercise with continuation value
-for i in range(8):
-    if price_matrix[2, i] < K:
+for i in range(paths):
+    if price_matrix[T-t, i] < K:
         # cont > ex --> t=3 is cf exercise, t=2 --> 0
-        if continuation_value[0, i] > max(0, (K-price_matrix[2, i])):
-            cf_matrix[3, i] = max(0, K-price_matrix[3, i])
-            cf_matrix[2, i] = 0
+        if continuation_value[0, i] > max(0, (K-price_matrix[T-t, i])):
+            cf_matrix[T, i] = max(0, K-price_matrix[T, i])      # todo: after T should be a minus sth for the loop e.g. T-t+1
+            cf_matrix[T-t, i] = 0
         # cont < ex --> t=3 is 0, t=2 immediate exercise
-        elif continuation_value[0, i] <= max(0, K-price_matrix[2, i]):
-            cf_matrix[2, i] = max(0, K-price_matrix[2, i])
-            cf_matrix[3, i] = 0
+        elif continuation_value[0, i] <= max(0, K-price_matrix[T-t, i]):
+            cf_matrix[T-t, i] = max(0, K-price_matrix[T-t, i])
+            cf_matrix[T, i] = 0
     # out of the money in t=2, t=2/3 both 0
     else:
-        cf_matrix[3, i] = 0
-        cf_matrix[2, i] = 0
-        print("3")
+        cf_matrix[T, i] = 0
+        cf_matrix[T-t, i] = 0
