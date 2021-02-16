2db84e1e79e6dc43f6235ca66bf84bef00760408
Casper Dik <casperdik3@gmail.com>
2021-02-16 10:29:47 +0100

    update

---
+++ practise/replicate_example_LSMC.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+""" 
+Here I have replicated the numerical example in chapter 1 of the paper by Longstaff and Schwartz(2001) 
+"""
+
+# stock price paths matrix
+# 8 paths, from t=0 to t=3
+price_matrix = np.array([[1, 1, 1, 1, 1, 1, 1, 1], [1.09, 1.16, 1.22, 0.93, 1.11, 0.76, 0.92, 0.88], [1.08, 1.26, 1.07, 0.97, 1.56, 0.77, 0.84, 1.22], [1.34, 1.54, 1.03, 0.92, 1.52, 0.9, 1.01, 1.34]])
+
+# cash flow matrix
+cf_matrix = np.zeros((4,8))
+
+# parameters
+K = 1.1
+rf = 0.06
+
+# cash flow matrix time 3
+for p in range(7):
+    cf_matrix[3, p] = max(0, K - (price_matrix[3, p]))
+
+cf_matrix.transpose()
+print(cf_matrix)
